6bfa2ea55896ef6bae90cb028af844c58ab61fb3
mountainsan <tks961020@gmail.com>
2022-02-09 17:52:07 +0900

    20220209 추가

---
+++ findahobby/src/main/java/com/acornacademy/Membership/Zipcode.java
@@ -0,0 +1,93 @@
+package com.acornacademy.Membership;
+
+//우편번호 검색 
+public class Zipcode {
+
+	// 구역번호
+	private String zipcode;
+
+	// 시도
+	private String sido;
+
+	// 시군구
+	private String sigungu;
+
+	// 읍면
+	private String ubmyeun;
+
+	// 도로명
+	private String doro;
+
+	// 법정동명
+	private String dong1;
+
+	// 리명
+	private String ri;
+
+	// 행정동명
+	private String dong2;
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getSido() {
+		return sido;
+	}
+
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+
+	public String getSigungu() {
+		return sigungu;
+	}
+
+	public void setSigungu(String sigungu) {
+		this.sigungu = sigungu;
+	}
+
+	public String getUbmyeun() {
+		return ubmyeun;
+	}
+
+	public void setUbmyeun(String ubmyeun) {
+		this.ubmyeun = ubmyeun;
+	}
+
+	public String getDoro() {
+		return doro;
+	}
+
+	public void setDoro(String doro) {
+		this.doro = doro;
+	}
+
+	public String getDong1() {
+		return dong1;
+	}
+
+	public void setDong1(String dong1) {
+		this.dong1 = dong1;
+	}
+
+	public String getRi() {
+		return ri;
+	}
+
+	public void setRi(String ri) {
+		this.ri = ri;
+	}
+
+	public String getDong2() {
+		return dong2;
+	}
+
+	public void setDong2(String dong2) {
+		this.dong2 = dong2;
+	}
+
